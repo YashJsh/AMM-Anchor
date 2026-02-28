6832cd7d6abcb6094e472fcaedc5722e78663392
yash <yaxj28@gmail.com>
2026-02-28 14:00:00 +0530

    Update helper functions and AMM logic

--- programs/amm/src/helper.rs
+++ programs/amm/src/helper.rs
@@ -1,5 +1,10 @@
-
 pub fn first_lp(amount_a : u64, amount_b : u64)-> u64{
     let product = amount_a * amount_b;
     product.isqrt()
+}
+
+pub fn calculate_lp(amount_a : u64, amount_b : u64, reserve_a : u64, reserve_b : u64, total_supply : u64)-> u64{
+    let lp_a = amount_a * total_supply /reserve_a;
+    let lp_b = amount_b * total_supply /reserve_b;
+    u64::min(lp_a, lp_b)
 }
--- programs/amm/src/lib.rs
+++ programs/amm/src/lib.rs
@@ -4,13 +4,16 @@ pub mod accounts;
 pub mod state;
 pub mod helper;
 
-use accounts::{AddLiquidity, InitializePool}
+use accounts::{AddLiquidity, InitializePool};
 
 
 declare_id!("HPEyUjDjeFVwGaetxFgq1qz9Wcq2dpGBnshPMXc6v8x9");
 
 #[program]
 pub mod amm {
+
+    use anchor_spl::token;
+
     use super::*;
 
     pub fn initialize(ctx: Context<InitializePool> , fee : u8) -> Result<()> {
@@ -29,8 +32,8 @@ pub mod amm {
         pool.lp_mint = ctx.accounts.lp_mint.key();
         pool.lp_mint_bump = ctx.bumps.lp_mint;
 
-        pool.vault_a = ctx.accounts.vault_token_A.key();
-        pool.vault_b = ctx.accounts.vault_token_B.key();
+        pool.vault_a = ctx.accounts.vault_token_a.key();
+        pool.vault_b = ctx.accounts.vault_token_b.key();
 
         pool.reserve_a = 0;
         pool.reserve_b = 0;
@@ -44,8 +47,14 @@ pub mod amm {
         //Check for the authority:
         require!(pool.authority == ctx.accounts.authority.key(), ErrorCode::InvalidAuthority);
 
-        //Check first, token_a != token_b
-        require!(ctx.accounts.user_token_a.key() != ctx.accounts.user_token_b.key(), ErrorCode::SimilarTokenError);
+        //Check first, token_a mint != token_b mint
+        require!(ctx.accounts.user_token_a.mint != ctx.accounts.user_token_b.mint, ErrorCode::SimilarTokenError);
+        
+
+        //Check that the token user is sending is equal to the token_a we have saved in the pool
+        require!(ctx.accounts.user_token_a.mint == pool.token_a, ErrorCode::DifferentTokenError);
+        require!(ctx.accounts.user_token_b.mint == pool.token_b, ErrorCode::DifferentTokenError);
+        require!(ctx.accounts.lp_mint.key() == pool.lp_mint, ErrorCode::InvalidMintAccount);
 
         //Check for the amount;
         require!(token_a_amount > 0 && token_b_amount > 0, ErrorCode::InvalidAmount);
@@ -54,43 +63,82 @@ pub mod amm {
         require!(pool.lp_mint == ctx.accounts.lp_mint.key(), ErrorCode::InvalidAccount);
         require!(pool.vault_a == ctx.accounts.vault_a.key() && pool.vault_b == ctx.accounts.vault_b.key(), ErrorCode::InvalidAccount);
 
+        let mut accepted_a = 0;
+        let mut accepted_b = 0;
+        //If reserves are 0 skip
+        if pool.reserve_a == 0 && pool.reserve_b == 0{
+            //Check that the user is providing liquidity in the ratio of 1:1
+            accepted_a = token_a_amount;
+            accepted_b = token_b_amount;
+        };
+
+        //If not first time : 
+        //Check for the ratio, and required 
+        if token_a_amount * reserve_b < token_b_amount * reserve_a{
+            //A is limiting
+            accepted_a = token_a_amount;
+            accepted_b = token_b_amount * pool.reserve_b / pool.reserve_a;
+        }else{
+            //B is limiting
+            accepted_b = token_b_amount;
+            accepted_a = token_a_amount * pool.reserve_a / pool.reserve_b;
+        }
+
+        //If accepted is 0; throw error
+        require!(accepted_a != 0 || accepted_b != 0, ErrorCode::InvalidAmount);
+
         //Transfer from user account to vault accounts;
         let cpi_account = Transfer{
             from : ctx.accounts.user_token_a.to_account_info(),
             to : ctx.accounts.vault_a.to_account_info(),
-            authority : ctx.accounts.authority.to_account_info(),
+            authority : ctx.accounts.payer.to_account_info()
         };
         let cpi_ctx = CpiContext::new(ctx.accounts.token_program.to_account_info(), cpi_account);
 
-        token::transfer(cpi_ctx, token_a_amount);
+        token::transfer(cpi_ctx, accepted_a)?;
 
         let cpi_account = Transfer{
             from : ctx.accounts.user_token_b.to_account_info(),
             to : ctx.accounts.vault_b.to_account_info(),
-            authority : ctx.accounts.authority.to_account_info(),
+            authority : ctx.accounts.payer.to_account_info(),
         };
         let cpi_ctx = CpiContext::new(ctx.accounts.token_program.to_account_info(), cpi_account);
 
-        token::transfer(cpi_ctx, token_b_amount);
-
-        //Transfer done. 
+        token::transfer(cpi_ctx, accepted_b)?;
         
         //Calculate the lp : 
         //Check if it the first lp:
+        let total_supply = ctx.accounts.lp_mint.supply;
+
+        let mut lp_amount = 0;
         if pool.reserve_a == 0 && pool.reserve_b == 0{
-            let lp_amount = helper::first_lp(token_a_amount, token_b_amount);
-            
+            lp_amount = helper::first_lp(token_a_amount, token_b_amount);
+        }else{
+            lp_amount = helper::calculate_lp(token_a_amount, token_b_amount, pool.reserve_a, pool.reserve_b, total_supply);
         }
 
+        require!(lp_amount > 0, ErrorCode::InvalidLpAmount);
 
-        
+        //Once we find the lp amount, we have to mint these no. of lp to the user's account.
+        let cpi_account = MintTo{
+            mint : ctx.accounts.lp_mint.to_account_info(),
+            authority : ctx.accounts.authority.to_account_info(),
+            to : ctx.accounts.user_lp_account.to_account_info()
+        };
 
+        //Now we have to create a context.
+        let signer_seeds = [
+            b"authority",
+            pool.key().as_ref(),
+            &[pool.authority_bump],
+        ];
+        let cpi_context = CpiContext::new_with_signer(ctx.accounts.token_program.to_account_info(), cpi_account, &[&signer_seeds]);
 
-        //Now update the reserves.
+        token::mint_to(cpi_context, lp_amount)?;
+
+         //Now update the reserves.
         pool.reserve_a += token_a_amount;
         pool.reserve_b += token_b_amount;
-
-
         Ok(())
     }
 }
@@ -108,5 +156,14 @@ pub enum ErrorCode{
     InvalidAmount,
 
     #[msg("Account doesn't match")]
-    InvalidAccount
+    InvalidAccount,
+
+    #[msg("Token's mint is not same as the pool's token")]
+    DifferentTokenError,
+
+    #[msg("Invalid lp amount calculated")]
+    InvalidLpAmount,
+
+    #[msg("Invalid mint account")]
+    InvalidMintAccount
 }
